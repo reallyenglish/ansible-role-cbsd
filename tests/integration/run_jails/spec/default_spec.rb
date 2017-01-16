5fad4ca1bbfaee64a1b09558ec3e5ed386ebb516
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-01-16 22:34:00 +0900

    add integration tests

--- tests/integration/run_jails/spec/default_spec.rb
+++ tests/integration/run_jails/spec/default_spec.rb
@@ -7,22 +7,16 @@ sleep 10 if ENV['JENKINS_HOME']
 
 context 'after provisioning finished' do
 
-  describe server(:client1) do
-
-    it 'should be able to ping server' do
-      result = current_server.ssh_exec("ping -c 1 #{ server(:server1).server.address } && echo OK")
-      expect(result).to match(/OK/)
-    end
-
-  end
-
   describe server(:server1) do
-
-    it 'should be able to ping client' do
-      result = current_server.ssh_exec("ping -c 1 #{ server(:client1).server.address } && echo OK")
-      expect(result).to match(/OK/)
+    it "gets base" do
+      result = current_server.ssh_exec("sudo env NOCOLOR=1 workdir=/usr/local/jails cbsd repo action=get sources=base")
+      expect(result).to match(/Done\.\.\./)
     end
 
+    it "starts a jail, foo" do
+      result = current_server.ssh_exec("sudo env NOCOLOR=1 workdir=/usr/local/jails cbsd jcreate jconf=/usr/local/etc/foo.jconf")
+      expect(result).to match(/^Creating foo complete: Enjoy!$/)
+    end
   end
 
 end
